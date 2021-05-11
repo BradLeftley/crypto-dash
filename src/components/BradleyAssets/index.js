84b41d3bd330457cd0fb6458ff409755c807d24e
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2021-05-11 14:52:29 +0100

    remove assets from news

---
+++ src/components/BradleyAssets/index.js
@@ -0,0 +1,59 @@
+import { Card } from 'antd';
+import { List, Avatar } from 'antd';
+import React, { useEffect, useState } from "react";
+
+function BradleyAssets (props){
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://covid-bradley.s3.eu-west-2.amazonaws.com/crypto.json")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+      }  
+      if (!isLoaded) {
+        return <div>Loading...</div>;
+      }  
+      console.log(items)
+
+      const gridStyle = {
+        width: '25%',
+        textAlign: 'center',
+      };
+
+    return (
+         <Card title="Assets">
+        <List
+    itemLayout="horizontal"
+    dataSource={items}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          title={<a href="https://ant.design">{item.asset}</a>}
+          description={item.free}
+        />
+      </List.Item>
+    )}
+  />
+        </Card>
+
+
+     
+        )
+}
+
+export default BradleyAssets;
--- src/components/News/index.js
+++ src/components/News/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card } from 'antd';
 import { Row, Col } from 'antd';
 import BlockchainBacker from '../BlockchainBacker';
+import BradleyAssets from '../BradleyAssets';
 const { Meta } = Card;
 
 function News() {
@@ -15,6 +16,9 @@ function News() {
         <Col className='gutter-row' xs={24} sm={12} md={8} lg={8} xl={6}>
           <iframe width="100%" scrolling="yes" allowtransparency="true" frameborder="0" src="https://cryptopanic.com/widgets/news/?bg_color=FFFFFF&amp;font_family=sans&amp;header_bg_color=30343B&amp;header_text_color=FFFFFF&amp;link_color=0091C2&amp;news_feed=recent&amp;text_color=333333&amp;title=Latest%20News" height="350px"></iframe>
         </Col>
+        {/* <Col className='gutter-row' xs={16} sm={12} md={8} lg={8} xl={6}>
+          <BradleyAssets />
+        </Col> */}
       </Row>
     </div>
   );
