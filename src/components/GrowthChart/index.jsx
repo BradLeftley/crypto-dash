56889018d24e00b461510f707a93c1d53aa699c0
Adam Thompson <adamthompson@192.168.0.225>
2021-05-16 21:27:06 +0100

    added filters to growthchart

--- src/components/GrowthChart/index.jsx
+++ src/components/GrowthChart/index.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {  Card, Button, Radio }from 'antd';
+import {  Card, Checkbox, Radio }from 'antd';
 import {useFetchAll} from '../../util/useFetch'
 import { Line } from '@ant-design/charts';
 import coinData from '../../util/coinData';
@@ -10,28 +10,42 @@ import coinData from '../../util/coinData';
 const createEndpoints = (days) =>{
     return coinData.map(coin=>`https://api.coingecko.com/api/v3/coins/${coin.id}/ohlc?vs_currency=usd&days=${days}`)
 }
+
+const createCheckBoxOptions = (data) => data.map(item => ({label: item.name, value: item.name}));
+const checkboxData = createCheckBoxOptions(coinData);
+
 const mapOHCLdata = (item, name, targetPrice) => {
   return item.map((data) => {
     const percentage = (((data[1] / targetPrice)) * 100)
     const roundedPercent = Math.round((percentage + Number.EPSILON) * 100) / 100;
     return { name, time: new Date(parseInt(data[0])).toISOString().split('T')[0], open: roundedPercent }
   })
-}
+};
+
+
 function GrowthChart(props) {
   const [timePeriod, setTimePeriod] = useState("90");
+  const [itemSelected, setItemsSelected] = useState(checkboxData.map(item => item.value))
   const { loading, error, data } = useFetchAll(createEndpoints(timePeriod),null,timePeriod);
 
   if (loading) return <p>loading...</p>;
   if (error) return <p>Error :(</p>;
   let testData = []
   data.forEach((item, index) => testData.push(...mapOHCLdata(item, coinData[index].name, coinData[index].target[0])))
-
+   
+  
    const handleSizeChange = e => {
         setTimePeriod( e.target.value);
     };
+    const checkBoxChange = (value) => {
+        setItemsSelected(value);
+    };
 
+    let filteredData = [];
+    itemSelected.forEach(item => filteredData.push(...testData.filter(filter => item === filter.name)));
+    
   var config = {
-    data: testData,
+    data: filteredData,
     xField: 'time',
     yField: 'open',
     seriesField: 'name',
@@ -54,6 +68,9 @@ function GrowthChart(props) {
           <Radio.Button value="180">6M</Radio.Button>
           <Radio.Button value="365">1Y</Radio.Button>
         </Radio.Group>
+        <br/>
+        <br/>
+        <Checkbox.Group defaultValue={itemSelected} options={checkboxData} onChange={checkBoxChange} />
        <Line {...config} />
     </Card>
   );
